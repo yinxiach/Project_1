a1785a9fea268bb8bdb6a4d90671063f77e5a31c
Calvin <yinxiach@gmail.com>
2018-06-30 21:56:53 +0800

    Test1

---
+++ src/test/GUI_test.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class GUI_test {
+	
+	@Test
+	public void test1() {
+		int a = 1;
+		int b = 2;
+		assertNotEquals(a,b);
+	}
+}
